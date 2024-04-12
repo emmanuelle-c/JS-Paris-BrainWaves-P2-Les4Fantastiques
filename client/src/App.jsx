41a0b6628f7d0bfc89c53badd068f8c61cecc281
Emmanuelle Curiant <emmanuelle.curiant@gmail.com>
2024-04-12 16:04:44 +0200

    test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,21 +2,19 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
+  const [hero, setHero] = useState(null);
 
-const [hero, setHero] = useState(null);
-console.log("hero");
-
-const getApi = () => {
-  fetch("https://akabab.github.io/superhero-api/api/all.json")
-    .then(response => response.json())
-    .then(data => setHero(data))
-}
+  const getApi = () => {
+    fetch("https://akabab.github.io/superhero-api/api/all.json")
+      .then((response) => response.json())
+      .then((data) => setHero(data));
+  };
   return (
     <>
-    <button type="button" onClick={getApi}>Get API</button>
-    { hero &&
-    hero.map((star) => <p key = {star.id}>{star.name}</p>)
-    }
+      <button type="button" onClick={getApi}>
+        Get API
+      </button>
+      {hero && hero.map((star) => <p key={star.id}>{star.name}</p>)}
     </>
   );
 }

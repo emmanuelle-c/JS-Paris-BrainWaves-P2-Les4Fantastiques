00a3917e965d1ff15b6bee99795ad76118b0d2fe
Emmanuelle Curiant <emmanuelle.curiant@gmail.com>
2024-04-16 18:57:49 +0200

    filtre par prix fonctionnel, ajout de dates pour chaque objet héro

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,8 @@ function App() {
   const [allHeroes, setAllHeroes] = useState([]);
   const [displayedHeroes, setDisplayedHeroes] = useState([]);
   const [startIndex, setStartIndex] = useState(12);
+  const [search, setSearch] = useState("");
+  const [filterHeroes, setFilterHeroes] = useState([]);
 
   // fetch les données API, suffle les données et afficher seulement 12 par défaut :
   useEffect(() => {
@@ -27,13 +29,14 @@ function App() {
         const shuffledHeroes = data.sort(() => Math.random() - 0.5); // fonction de comparaison aléatoire
         setAllHeroes(shuffledHeroes);
         setDisplayedHeroes(shuffledHeroes.slice(0, 12)); // charger les 12 premiers héros du tableau qui a été mélangé
+        setFilterHeroes(shuffledHeroes);
       });
   }, []);
 
   // ajouter des héros en affichage 12 par 12 :
   const loadMoreHeroes = () => {
     const endIndex = startIndex + 12; // calculer index de fin pour les suivants
-    const nextHeroes = allHeroes.slice(startIndex, endIndex); // extraire les héros suivants de la liste complète
+    const nextHeroes = filterHeroes.slice(startIndex, endIndex); // extraire les héros suivants de la liste complète
     // (prevHeroes)contient les premiers héros déjà affichés
     setDisplayedHeroes((prevHeroes) => [...prevHeroes, ...nextHeroes]); // cette syntaxe(...) permet de fusionner les héros précédents avec les suivants sans écraser les 1ers
     setStartIndex(endIndex); // mettre à jour index de départ pour clic suivant
@@ -45,16 +48,24 @@ function App() {
       hero.powerstats.durability + hero.powerstats.strength
     );
   });
+  // ajouter une date random à chaque objet :
+  function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+  }
+
+  allHeroes.forEach(function (hero){
+    hero.date = randomDate(new Date(2024, 12, 31), new Date());
+  })
+
   // faire une recherche par prix :
-  const [search, setSearch] = useState("");
-  const [filterHeroes, setFilterHeroes] = useState(displayedHeroes);
 
   const handleClick = () => {
-    const filter = allHeroes.filter((hero) => hero.price = search);
-    if (filter.length != 0) {
+    const filter = allHeroes.filter((hero) => hero.price >= Number(search));
+    if (filter.length === 0) {
       setFilterHeroes(displayedHeroes);
     } else {
       setFilterHeroes(filter);
+      setDisplayedHeroes(filter.slice(0, 12));
     }
   };
 
@@ -71,7 +82,7 @@ function App() {
         />
       </div>
       <div className="hero-container">
-        {filterHeroes.map((hero) => (
+        {displayedHeroes.map((hero) => (
           <div className="hero-card" key={hero.id}>
             <HeroCard hero={hero} />
           </div>

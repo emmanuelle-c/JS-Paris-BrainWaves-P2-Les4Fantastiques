e6106459a76d4859908a4abb474b1b4746a73fc7
Coline1186 <coline.grosso@neuf.fr>
2024-04-19 09:09:39 +0200

    responsive mobile

--- client/src/pages/FirstPage/FirstPage.jsx
+++ client/src/pages/FirstPage/FirstPage.jsx
@@ -1,10 +1,17 @@
 import "./FirstPage.css"
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaRegUser } from "react-icons/fa6";
 import ban from "../../assets/images/logos/banniere.svg"
 import logo from "../../assets/images/logos/logo-fond-0.png"
 
 
-function FirstPage() {
+function FirstPage() {  
+  const [showLog, setShowLog] = useState(false);
+
+  const handleShowLog = () => {
+    setShowLog(!showLog);
+  };
   return (
     <section className='background'>
     <div className='content'> 
@@ -13,11 +20,25 @@ function FirstPage() {
         <button type='button' className='login'>LOGIN</button>
         <button type='button' className='sign'>SIGN IN</button>
     </div>
-    <img src={ban} className='banniere' alt="banniere"/>
-    <p className='text-entry'><span>Rendez votre quotidien extraordinaire</span> <br/>déménagements sans stress, cours de sport dynamiques, animations d'anniversaires mémorables et bien plus encore...Louez dès aujourd'hui et vivez l'aventure avec nos héros à vos côtés!</p>    
-    <Link to ="/accueil">
-    <button type="button" className='button-entry'>DISCOVER THE EXPERIENCE</button>
-    </Link>
+    <div className="user-responsive">
+        <img src={logo} className='logo-responsive' alt="logo-responsive" />
+    <div className="button-responsive">
+        <button type="button" aria-label="button" className="button-user" onClick={handleShowLog} onKeyDown={handleShowLog} >
+          <FaRegUser className="user" />
+        </button>
+        {showLog &&(
+        <ul className="log-responsive">
+        <button type='button' className='hide-login'>LOGIN</button>
+        <button type='button' className='hide-sign'>SIGN IN</button>
+        </ul>
+        )}
+    </div>
+    </div>
+        <img src={ban} className='banniere' alt="banniere"/>
+        <p className='text-entry'><span>Rendez votre quotidien extraordinaire</span> <br/>déménagements sans stress, cours de sport dynamiques, animations d'anniversaires mémorables et bien plus encore...Louez dès aujourd'hui et vivez l'aventure avec nos héros à vos côtés!</p>    
+        <Link to ="/accueil">
+        <button type="button" className='button-entry'>DISCOVER THE EXPERIENCE</button>
+        </Link>
     </div>
     </section>
   )

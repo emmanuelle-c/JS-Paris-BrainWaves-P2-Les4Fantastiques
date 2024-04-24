cd82c8b7e2c6381b39dfead3818f584a8931f7bc
Coline1186 <coline.grosso@neuf.fr>
2024-04-24 16:39:27 +0200

    Adam's work recovered

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -102,8 +102,7 @@ function App() {
   const handleName =(e) => {
     setSearchName(e.target.value)
     const filterByName = allHeroes.filter(hero => hero.name.toLowerCase().includes(e.target.value.toLowerCase()))
-      setSearchName && 
-        setFilterHeroes(filterByName); 
+      setSearchName && setFilterHeroes(filterByName); 
         setDisplayedHeroes(filterByName.slice(0, 12));
   }
   return (

0f8f26ec8053da3d6c26dfa6d1577e3d9d2dfca2
Coline1186 <coline.grosso@neuf.fr>
2024-04-29 11:30:49 +0200

    push before merge between us

--- client/src/pages/PageHero/PageHero.jsx
+++ client/src/pages/PageHero/PageHero.jsx
@@ -3,10 +3,10 @@ import axios from "axios";
 import "./PageHero.css";
 import "./PageHeroResponsive.css";
 import { useParams } from "react-router-dom";
-import Header from "../components/Header/Header";
-import AvisClient from "../components/Comment/AvisClient";
-import Formulaire from "../components/Formulaire/Formulaire";
-import Footer from "../components/Footer/Footer"
+import Header from "../../components/Header/Header";
+import AvisClient from "../../components/Comment/AvisClient";
+import Formulaire from "../../components/Formulaire/Formulaire";
+import Footer from "../../components/Footer/Footer"
 
 function PageHero() {
   const [superhero, setSuperhero] = useState(null);
